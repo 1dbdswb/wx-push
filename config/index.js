e37b85d0c9479ace361c1f2f27d603be9fd64f8a
第一眼心动 <57927050+1dbdswb@users.noreply.github.com>
2024-07-18 17:41:41 +0800

    index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx6183a574d0b7b684",
   appsecret: "ce3382c721650df4630369d078c32dc2",
   template_id: "EtcyeUs5Q_S8OK_W5ODUbWKX6LemkCMh3_JTZXOK39Y",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "odZux6SC4B6MHhoJ4ai2NtKeqSJo",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "巩义",
